348128e2a7691a1ec3d393af71ed4fe1ec70fbb7
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-06-16 22:59:56 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int oneMile = 20;
+        int totalMiles = ticketPrice / oneMile;
+        System.out.printf("Количество миль = " + totalMiles);
+    }
+}
